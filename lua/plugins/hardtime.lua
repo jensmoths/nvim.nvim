b38b13fed8b1ad6de7abe304bd0983aba97deaf7
Jens Moths <jens.moths@gmail.com>
2025-04-12 11:44:58 +0200

    tune hardtime and disable context

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,7 +3,22 @@ return {
     {
       'm4xshen/hardtime.nvim',
       dependencies = { 'MunifTanjim/nui.nvim' },
-      opts = { disable_mouse = false },
+      opts = {
+        max_count = 10,
+        disable_mouse = false,
+        disabled_keys = {
+          ['<Up>'] = {},
+          ['<Down>'] = {},
+          ['<Left>'] = {},
+          ['<Right>'] = {},
+        },
+        restricted_keys = {
+          ['<Up>'] = { '', 'i' },
+          ['<Down>'] = { '', 'i' },
+          ['<Left>'] = { '', 'i' },
+          ['<Right>'] = { '', 'i' },
+        },
+      },
     },
   },
 }
